07032750fe7afd4e2ebd65ab3f95763e2b69a0f6
haiqal <haiqalfawaz14@gmail.com>
2025-01-23 23:48:58 +0700

    update homepage

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,13 +1,21 @@
 import Navbar from "@/components/Navbar";
+import Postbar from "@/components/Postbar";
+import Storybar from "@/components/Storybar";
 import Image from "next/image";
 import React from "react";
+import MiniProfile from "@/components/Miniprofile";
 
 const HomePage = () => {
   return (
-    <div className="bg-anastasia-1 h-screen p-5">
-      <div className="flex justify-center items-start gap-10">
-        <Image src="/Logo-crop.png" alt="Logo" width={200} height={200} />
+    <div className="bg-anastasia-1 h-screen p-5 flex flex-col justify-center items-center gap-5">
+      <div className="flex justify-center items-start gap-20 w-full">
+        <Image src="/Logo-crop.png" alt="Logo" width={150} height={150} />
         <Navbar />
+      </div>
+      <div className="w-full flex justify-start items-start gap-5">
+        <Storybar />
+        <Postbar />
+        <MiniProfile />
       </div>
     </div>
   );
