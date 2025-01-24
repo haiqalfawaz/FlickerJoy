7997a5c4fd862398544c8ac508341ed2d9950e37
haiqal <haiqalfawaz14@gmail.com>
2025-01-24 18:47:44 +0700

    update and integrate API of get following posts with SSR method

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -10,17 +10,26 @@ export async function getServerSideProps(context) {
   try {
     const token = context.req.cookies.token || "";
     const apiURL = process.env.NEXT_PUBLIC_API_URL;
+    const apiKEY = process.env.NEXT_PUBLIC_API_KEY;
 
     const UserRes = await axios.get(`${apiURL}/user`, {
       headers: {
         Authorization: `Bearer ${token}`,
-        apiKey: process.env.NEXT_PUBLIC_API_KEY ?? "",
+        apiKey: apiKEY ?? "",
       },
     });
-    console.log(UserRes.data);
+
+    const PostRes = await axios.get(`${apiURL}/following-post?size=10&page=1`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        apiKey: apiKEY ?? "",
+      },
+    });
+
     return {
       props: {
         user: UserRes.data.data,
+        posts: PostRes.data.data.posts,
       },
     };
   } catch (error) {
@@ -28,12 +37,13 @@ export async function getServerSideProps(context) {
     return {
       props: {
         user: null,
+        posts: [],
       },
     };
   }
 }
 
-const HomePage = ({ user }) => {
+const HomePage = ({ user, posts }) => {
   return (
     <div className="bg-anastasia-1 h-screen p-5 flex flex-col justify-center items-center gap-5">
       <div className="flex justify-center items-start gap-20 w-full">
@@ -42,7 +52,7 @@ const HomePage = ({ user }) => {
       </div>
       <div className="w-full flex justify-start items-start gap-5">
         <Storybar />
-        <Postbar />
+        <Postbar posts={posts} />
         <MiniProfile user={user} />
       </div>
     </div>
