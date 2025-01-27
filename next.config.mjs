6de2c76fb37d3e8ab5b79cb02e6162c4bc428d25
haiqal <haiqalfawaz14@gmail.com>
2025-01-27 22:09:36 +0700

    add another domains image url

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,10 @@ const nextConfig = {
       "photo-sharing-api-bootcamp.do.dibimbing.id",
       "depositphotos-blog.s3.eu-west-1.amazonaws.com",
       "cdn1-production-images-kly.akamaized.net",
+      "upload.wikimedia.org",
+      "www.blibli.com",
+      "static.republika.co.id",
+      "www.google.com",
     ],
   },
 };
