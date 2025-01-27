d597e7cd2e41e2e7cbf290bd245c13fbcb3328d3
haiqal <haiqalfawaz14@gmail.com>
2025-01-27 18:20:17 +0700

    edit photo url

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      "www.masakapahariini.com",
+      "images.unsplash.com",
+      "photo-sharing-api-bootcamp.do.dibimbing.id",
+    ],
+  },
 };
 
 export default nextConfig;
