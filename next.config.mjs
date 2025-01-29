6d5d3ff21f0239e56b17a3b4028b58ec35e8d54a
haiqal <haiqalfawaz14@gmail.com>
2025-01-29 21:17:50 +0700

    update config remote patterns

--- next.config.mjs
+++ next.config.mjs
@@ -2,18 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: [
-      "www.masakapahariini.com",
-      "images.unsplash.com",
-      "photo-sharing-api-bootcamp.do.dibimbing.id",
-      "depositphotos-blog.s3.eu-west-1.amazonaws.com",
-      "cdn1-production-images-kly.akamaized.net",
-      "upload.wikimedia.org",
-      "www.blibli.com",
-      "static.republika.co.id",
-      "www.google.com",
-      "www.nestleprofessional.co.id",
-      "encrypted-tbn0.gstatic.com",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "photo-sharing-api-bootcamp.do.dibimbing.id",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
