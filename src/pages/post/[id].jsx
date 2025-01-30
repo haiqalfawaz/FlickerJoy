4e52129a38c6861cccaffef8aabfc3369e9a9504
haiqal <haiqalfawaz14@gmail.com>
2025-01-30 17:38:59 +0700

    adujusting create comment for detail post

--- src/pages/post/[id].jsx
+++ src/pages/post/[id].jsx
@@ -1,11 +1,14 @@
 import Navbar from "@/components/Navbar";
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import Image from "next/image";
+import useComment from "@/hooks/useComment";
 
 // Import Icons
 import { GoHeart } from "react-icons/go";
 import { FaRegCommentAlt } from "react-icons/fa";
+import { GoHeartFill } from "react-icons/go";
+import { IoSend } from "react-icons/io5";
 
 export async function getServerSideProps(context) {
   const { id } = context.params;
@@ -38,50 +41,118 @@ export async function getServerSideProps(context) {
 }
 
 const PostDetailsPage = ({ postDetails }) => {
+  const {
+    comments,
+    comment,
+    setComment,
+    sendComment,
+    isLoading,
+    error,
+    handleCommentChange,
+    setComments,
+  } = useComment();
+
+  useEffect(() => {
+    if (postDetails.comments) {
+      setComments(postDetails.comments);
+    }
+  }, [postDetails, setComments]);
+
   return (
     <div className="bg-anastasia-1 h-screen p-5 flex flex-col justify-center items-center gap-5">
       <div className="flex justify-center items-start gap-20 w-full">
         <Image src="/Logo-crop.png" alt="Logo" width={150} height={150} />
         <Navbar />
       </div>
-      <div className="w-full p-2 border border-dashed rounded-lg border-black">
+      <div className="w-full p-2 border-2 border-dashed rounded-lg border-black h-full">
         <div className="flex justify-center items-start h-full gap-7 mb-2">
-          <div className="flex flex-col justify-center items-center gap-10">
+          <div className="flex flex-col justify-center items-center w-1/2 gap-4">
             <Image
               src={postDetails.imageUrl || "/user.png"}
               alt="Post"
-              width={400}
-              height={400}
-              className="border-2 rounded-xl border-black"
+              width={700}
+              height={700}
+              className="border-2 rounded-xl border-black object-cover h-[450px] w-[700px]"
             />
-            <div className="flex justify-center items-center gap-10">
-              <button className="bg-anastasia-2 rounded-lg [box-shadow:5px_5px_black] border border-black p-2 text-2xl text-black">
-                <GoHeart /> {/* button like */}
-              </button>
-              <button className="bg-anastasia-2 rounded-lg [box-shadow:5px_5px_black] border border-black p-2 text-2xl text-black">
-                <FaRegCommentAlt />
-              </button>
+            <div className="flex justify-center items-center gap-5">
+              <div className="flex justify-center items-center gap-2 py-1 px-2 bg-anastasia-2 rounded-lg border border-black [box-shadow:5px_5px_black]">
+                <button className="text-black font-bold text-5xl">
+                  {postDetails.isLike === true ? <GoHeartFill /> : <GoHeart />}
+                </button>
+                <p className="text-xl text-black">{postDetails.totalLikes}</p>
+              </div>
+              <div className="flex justify-center items-center gap-4 p-1 bg-anastasia-2 rounded-lg border border-black [box-shadow:5px_5px_black]">
+                <button className="text-black text-5xl">
+                  <FaRegCommentAlt />
+                </button>
+              </div>
+              <div className="flex justify-center items-center gap-4 p-1 bg-anastasia-2 rounded-lg border border-black [box-shadow:5px_5px_black] h-[59px]">
+                <h3 className="text-black text-2xl">
+                  {new Date(postDetails.createdAt).toLocaleDateString("id-ID")}
+                </h3>
+              </div>
+              <div className="flex justify-center items-center gap-4 p-1 bg-anastasia-2 rounded-lg border border-black [box-shadow:5px_5px_black]">
+                <Image
+                  src={postDetails.user.profilePictureUrl || "/user.png"}
+                  width={50}
+                  height={50}
+                  className="border border-black rounded-full"
+                />
+                <h3 className="text-black font-semibold text-2xl">
+                  @{postDetails.user.username}
+                </h3>
+              </div>
+            </div>
+            <div className="bg-anastasia-2 border border-black [box-shadow:5px_5px_black] rounded-lg p-2 w-full text-center">
+              <p className="text-xl text-black">{postDetails.caption}</p>
             </div>
           </div>
-          <div className="flex flex-col justify-center items-center gap-4">
-            <div className="bg-anastasia-2 rounded-lg [box-shadow:5px_5px_black] border border-black p-2 text-2xl text-black w-full text-center">
-              <p>
-                {new Date(postDetails.createdAt).toLocaleDateString("id-ID")}
-              </p>{" "}
-              {/* created at */}
+          <div className="bg-anastasia-4 rounded-lg [box-shadow:5px_5px_black] h-full w-1/2 border-2 border-anastasia-2 py-3 px-9 flex flex-col justify-center items-center">
+            <div className="mb-3">
+              <h1 className="text-center font-bold text-3xl text-anastasia-2">
+                Comments
+              </h1>
             </div>
-            <div className="bg-anastasia-2 rounded-lg [box-shadow:5px_5px_black] border border-black p-2 text-2xl text-black flex justify-center items-center gap-2 w-full">
-              <Image
-                src={postDetails.user.profilePictureUrl || "/user.png"}
-                alt="user"
-                width={50}
-                height={50}
-                className="border-2 rounded-xl border-black"
+            <div className="border-2 border-anastasia-2 p-2 rounded-lg w-full h-full mb-3 flex flex-col items-center overflow-auto text-anastasia-2 gap-3">
+              {postDetails.comments && postDetails.comments.length > 0 ? (
+                postDetails.comments.map((comment) => (
+                  <div
+                    key={comment.id}
+                    className="flex justify-start items-center w-full"
+                  >
+                    <Image
+                      src={comment.user.profilePictureUrl || "/user.png"}
+                      alt="user"
+                      width={40}
+                      height={40}
+                      className="rounded-full border border-anastasia-2 mr-2"
+                    />
+                    <p className="text-xl font-semibold mr-4">
+                      @{comment.user.username} :
+                    </p>
+                    <p className="text-base">{comment.comment}</p>
+                  </div>
+                ))
+              ) : (
+                <div className="flex justify-center items-center">
+                  <p>Be the first to send a message!</p>
+                </div>
+              )}
+            </div>
+            <div className="gap-2 flex justify-center items-center border border-anastasia-2 p-2 rounded-lg w-full">
+              <input
+                type="text"
+                placeholder="Send a comment!"
+                className="bg-transparent focus:outline-none w-full text-center"
+                onChange={handleCommentChange}
+                name="comment"
               />
-              <p>@{postDetails.user.username}</p> {/* username */}
-            </div>
-            <div className="bg-anastasia-2 rounded-lg [box-shadow:5px_5px_black] border border-black p-2 text-xl text-black h-96 w-72 flex justify-center items-center text-center">
-              {postDetails.caption} {/* caption */}
+              <button
+                onClick={() => sendComment(postDetails.id)}
+                disabled={isLoading}
+              >
+                <IoSend />
+              </button>
             </div>
           </div>
         </div>
