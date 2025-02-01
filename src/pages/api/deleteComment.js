63bac5eeb1f52bec48862c4373d1ba7c6ee9ac69
haiqal <haiqalfawaz14@gmail.com>
2025-02-01 14:00:00 +0700

    Integrate API of delete comment

---
+++ src/pages/api/deleteComment.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import React from "react";
+
+const deleteCommentAPI = async (req, res) => {
+  const token = req.cookies.token || "";
+  if (req.method === "DELETE") {
+    try {
+      const { commentId } = req.query;
+      const apiURL = process.env.NEXT_PUBLIC_API_URL;
+      const apiKEY = process.env.NEXT_PUBLIC_API_KEY;
+
+      const response = await axios.delete(
+        `${apiURL}/delete-comment/${commentId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            apiKey: apiKEY ?? "",
+          },
+        }
+      );
+      res.status(200).json(response.data);
+    } catch (error) {
+      console.error("Error Delete Comment:", error);
+      res.status(500).json({ error: "failed to detele comment" });
+    }
+  } else {
+    res.status(405).json({ error: "Method not allowed" });
+  }
+};
+
+export default deleteCommentAPI;
