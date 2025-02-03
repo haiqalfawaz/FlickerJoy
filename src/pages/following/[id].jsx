0111788775f6cb6d83d12f87214065a62ccef5cd
haiqal <haiqalfawaz14@gmail.com>
2025-02-03 13:52:28 +0700

    update infinite scroll for following page

--- src/pages/following/[id].jsx
+++ src/pages/following/[id].jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Navbar from "@/components/Navbar";
 import Image from "next/image";
 import axios from "axios";
 import { useRouter } from "next/router";
+import { getCookies } from "cookies-next";
 
 export async function getServerSideProps(context) {
   try {
@@ -10,10 +11,9 @@ export async function getServerSideProps(context) {
     const token = context.req.cookies.token || "";
     const apiURL = process.env.NEXT_PUBLIC_API_URL;
     const apiKEY = process.env.NEXT_PUBLIC_API_KEY;
-    const { page = 1, size = 10 } = context.query;
 
     const FollowingRes = await axios.get(
-      `${apiURL}/following/${id}?size=${size}&page=${page}`,
+      `${apiURL}/following/${id}?size=10&page=1`,
       {
         headers: {
           Authorization: `Bearer ${token}`,
@@ -23,28 +23,97 @@ export async function getServerSideProps(context) {
     );
 
     const followings = FollowingRes.data.data.users;
+    const totalPages = FollowingRes.data.data.totalPages;
 
     return {
       props: {
         followings,
+        totalPages,
       },
     };
   } catch (error) {
     console.error("Error Fetching Data:", error);
     return {
       props: {
-        followers: [],
+        following: [],
+        totalPages: 0,
       },
     };
   }
 }
 
-const FollowersPage = ({ followings }) => {
+const FollowingPage = ({ followings, totalPages }) => {
   const router = useRouter();
+  const [currentFollowing, setCurrentFollowing] = useState(followings);
+  const [page, setpage] = useState(1);
+  const [hasMore, setHasMore] = useState(totalPages > 1);
+  const [loading, setLoading] = useState(false);
+  const containerRef = useRef(null);
 
   const handleClickCard = (followingId) => {
     router.push(`/users/${followingId}`);
   };
+
+  const loadMoreFollowing = async () => {
+    if (loading || !hasMore) return;
+
+    setLoading(true);
+
+    try {
+      const token = getCookies().token;
+      const apiURL = process.env.NEXT_PUBLIC_API_URL;
+      const apiKEY = process.env.NEXT_PUBLIC_API_KEY;
+
+      const res = await axios.get(
+        `${apiURL}/following/${router.query.id}?size=10&page=${page + 1}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            apiKEY: apiKEY ?? "",
+          },
+        }
+      );
+
+      console.log("Following ID:", router.query.id);
+      const newFollowing = res.data.data.users;
+
+      setCurrentFollowing((prev) => [...prev, ...newFollowing]);
+
+      if (newFollowers.length < 10) {
+        setHasMore(false);
+      }
+      setpage((prevPage) => prevPage + 1);
+    } catch (error) {
+      console.error("Error loading more followers:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const container = containerRef.current;
+      if (!container) return;
+
+      if (
+        container.scrollTop + container.clientHeight >=
+        container.scrollHeight - 1
+      ) {
+        loadMoreFollowers();
+      }
+    };
+
+    const container = containerRef.current;
+    if (container) {
+      container.addEventListener("scroll", handleScroll);
+    }
+
+    return () => {
+      if (container) {
+        container.removeEventListener("scroll", handleScroll);
+      }
+    };
+  }, [page, hasMore, loading]);
 
   return (
     <div className="bg-anastasia-1 h-screen p-5 flex flex-col justify-center items-center gap-5">
@@ -52,11 +121,14 @@ const FollowersPage = ({ followings }) => {
         <Image src="/Logo-crop.png" alt="Logo" width={150} height={150} />
         <Navbar />
       </div>
-      <div className="w-full border-2 border-dashed h-full rounded-lg border-black place-items-center p-4 overflow-y-auto">
+      <div
+        ref={containerRef}
+        className="w-full border-2 border-dashed h-full rounded-lg border-black place-items-center p-4 overflow-y-auto"
+      >
         {/* Card Followings */}
-        {followings.length > 0 ? (
+        {currentFollowing.length > 0 ? (
           <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 p-1">
-            {followings.map((following) => (
+            {currentFollowing.map((following) => (
               <div
                 key={following.id}
                 onClick={() => handleClickCard(following.id)}
@@ -64,7 +136,7 @@ const FollowersPage = ({ followings }) => {
               >
                 <Image
                   src={following.profilePictureUrl || "/user.png"}
-                  alt="followings"
+                  alt="followers"
                   width={200}
                   height={200}
                   className="rounded-lg border border-black"
@@ -76,11 +148,11 @@ const FollowersPage = ({ followings }) => {
             ))}
           </div>
         ) : (
-          <p>No Followings Found</p>
+          <p>No followingFound</p>
         )}
       </div>
     </div>
   );
 };
 
-export default FollowersPage;
+export default FollowingPage;
