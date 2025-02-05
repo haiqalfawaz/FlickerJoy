f62aa2934049c5f1279d066a8a59dbfa1e774a5b
haiqal <haiqalfawaz14@gmail.com>
2025-02-05 12:23:03 +0700

    create custom hooks  create post for user creating post

---
+++ src/hooks/useCreatePost.js
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import { getCookies } from "cookies-next";
+import axios from "axios";
+
+const useCreatePost = () => {
+  const [urlImage, setUrlImage] = useState(null);
+  const [postData, setPostData] = useState({ caption: "" });
+  const [uploadLoading, setUploadLoading] = useState(false);
+  const [postLoading, setPostLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const handleChange = (e) => {
+    setPostData({
+      ...postData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleUploadImage = async (imageFile) => {
+    const token = getCookies().token;
+    const apiURL = process.env.NEXT_PUBLIC_API_URL;
+    const apiKEY = process.env.NEXT_PUBLIC_API_KEY;
+
+    const formData = new FormData();
+    formData.append("image", imageFile);
+
+    setUploadLoading(true);
+
+    try {
+      const res = await axios.post(`${apiURL}/upload-image`, formData, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          apiKey: apiKEY ?? "",
+        },
+      });
+
+      console.log("Image uploaded successfully. URL:", res.data.url);
+      setUrlImage(res.data.url);
+    } catch (error) {
+      setError("Error to get URL");
+    } finally {
+      setUploadLoading(false);
+    }
+  };
+
+  const handleCreatePost = async () => {
+    const token = getCookies().token;
+    const apiURL = process.env.NEXT_PUBLIC_API_URL;
+    const apiKEY = process.env.NEXT_PUBLIC_API_KEY;
+
+    if (!urlImage || !postData.caption) {
+      console.log("missing imageURL and Captions!");
+      return;
+    }
+
+    const payload = {
+      imageUrl: urlImage,
+      caption: postData.caption,
+    };
+
+    console.log(payload);
+
+    setPostLoading(true);
+    setError(null);
+
+    try {
+      const res = await axios.post(`${apiURL}/create-post`, payload, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          apiKey: apiKEY ?? "",
+        },
+      });
+
+      console.log(res.data);
+    } catch (error) {
+      setError(error.response ? error.response.data.error : error.message);
+    } finally {
+      setPostLoading(false);
+    }
+  };
+
+  return {
+    handleUploadImage,
+    handleCreatePost,
+    handleChange,
+    urlImage,
+    postData,
+    uploadLoading,
+    postLoading,
+    error,
+  };
+};
+
+export default useCreatePost;
