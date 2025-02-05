a127d6e01c0f4c954590fe54ce318cf0b3f34fc1
haiqal <haiqalfawaz14@gmail.com>
2025-02-05 12:23:20 +0700

    create custom hooks  create story for user creating story

---
+++ src/hooks/useCreateStory.js
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import { getCookies } from "cookies-next";
+import axios from "axios";
+
+const useCreateStory = () => {
+  const [urlImageStory, setUrlImageStory] = useState(null);
+  const [storyData, setStoryData] = useState({ caption: "" });
+  const [uploadStoryLoading, setUploadStoryLoading] = useState(false);
+  const [storyLoading, setStoryLoading] = useState(false);
+  const [errorStory, setErrorStory] = useState(null);
+
+  const handleChangeCaption = (e) => {
+    setStoryData({
+      ...storyData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleUploadImageStory = async (imageFile) => {
+    const token = getCookies().token;
+    const apiURL = process.env.NEXT_PUBLIC_API_URL;
+    const apiKEY = process.env.NEXT_PUBLIC_API_KEY;
+
+    const formData = new FormData();
+    formData.append("image", imageFile);
+
+    setUploadStoryLoading(true);
+
+    try {
+      const res = await axios.post(`${apiURL}/upload-image`, formData, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          apiKey: apiKEY ?? "",
+        },
+      });
+
+      console.log("Image uploaded successfully. URL:", res.data.url);
+      setUrlImageStory(res.data.url);
+    } catch (error) {
+      setError("Error to get URL");
+    } finally {
+      setUploadStoryLoading(false);
+    }
+  };
+
+  const handleCreateStory = async () => {
+    const token = getCookies().token;
+    const apiURL = process.env.NEXT_PUBLIC_API_URL;
+    const apiKEY = process.env.NEXT_PUBLIC_API_KEY;
+
+    if (!urlImageStory || !storyData.caption) {
+      console.log("missing imageURL and Captions!");
+      return;
+    }
+
+    const payload = {
+      imageUrl: urlImageStory,
+      caption: storyData.caption,
+    };
+
+    console.log(payload);
+
+    setStoryLoading(true);
+    setErrorStory(null);
+
+    try {
+      const res = await axios.post(`${apiURL}/create-story`, payload, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          apiKey: apiKEY ?? "",
+        },
+      });
+
+      console.log(res.data);
+    } catch (error) {
+      setError(error.response ? error.response.data.error : error.message);
+    } finally {
+      setStoryLoading(false);
+    }
+  };
+
+  return {
+    handleUploadImageStory,
+    handleCreateStory,
+    handleChangeCaption,
+    urlImageStory,
+    storyData,
+    storyLoading,
+  };
+};
+
+export default useCreateStory;
