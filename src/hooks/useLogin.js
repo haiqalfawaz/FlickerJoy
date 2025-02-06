b9a01991e9837b4239d394cbc236192658dede9b
haiqal <haiqalfawaz14@gmail.com>
2025-02-06 17:56:06 +0700

    update state at custom hooks useLogin

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -5,6 +5,9 @@ import axios from "axios";
 
 const useLogin = () => {
   const router = useRouter();
+  const [loading, setloading] = useState(false);
+  const [success, setSuccess] = useState("");
+  const [error, setError] = useState(null);
   const [formData, setFormData] = useState({
     email: "",
     password: "",
@@ -18,6 +21,8 @@ const useLogin = () => {
   };
 
   const handleLogin = async () => {
+    setloading(true);
+    setError(null);
     try {
       const res = await axios.post("/api/authentication/login", formData, {
         headers: {
@@ -26,12 +31,24 @@ const useLogin = () => {
       });
       console.log(res.data.message);
       setCookie("token", res.data.token);
+      setSuccess("Login Successful!");
       router.push("/home");
     } catch (error) {
+      setError("Email or Password Wrong");
       console.log("Login Failed", error.message);
+    } finally {
+      setloading(false);
     }
   };
-  return { handleLogin, handleChange, formData, setFormData };
+  return {
+    handleLogin,
+    handleChange,
+    formData,
+    setFormData,
+    loading,
+    success,
+    error,
+  };
 };
 
 export default useLogin;
