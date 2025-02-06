cacd06bce6c1c8e9c611dc7adf908000a4430b8d
haiqal <haiqalfawaz14@gmail.com>
2025-02-06 17:57:31 +0700

    update responsif at login page and form login

--- src/components/FormLogin/index.jsx
+++ src/components/FormLogin/index.jsx
@@ -5,9 +5,11 @@ import Link from "next/link";
 //Import Icons
 import { FaEye } from "react-icons/fa";
 import { FaEyeSlash } from "react-icons/fa";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 const FormLogin = () => {
-  const { handleLogin, handleChange, formData } = useLogin();
+  const { handleLogin, handleChange, formData, loading, error, success } =
+    useLogin();
   const [showPassword, setShowPassword] = useState(false);
 
   const handleShowPassword = () => {
@@ -17,27 +19,33 @@ const FormLogin = () => {
   const isButtonDisabled = !formData.email || !formData.password;
 
   return (
-    <div className="flex flex-col justify-start items-center h-[340px] w-full lg:w-fit lg:h-full lg:px-[72px] px-[53px] lg:py-[90px] bg-anastasia-2 border rounded-3xl [box-shadow:5px_5px_black]">
-      <h1 className="font-bold text-8xl mb-20">LOGIN</h1>
-      <div className="text-center flex flex-col items-center gap-2 mb-7">
+    <div className="flex flex-col justify-start items-center h-auto w-full lg:w-fit lg:h-auto lg:px-[72px] px-[16px] lg:py-[90px] py-[20px] bg-anastasia-2 border rounded-3xl shadow-[5px_5px_0_black]">
+      <h1 className="font-extrabold tracking-tight lg:text-8xl text-4xl lg:mb-20 mb-3">
+        LOGIN
+      </h1>
+
+      {/* Email Field */}
+      <div className="text-center flex flex-col items-center gap-2 lg:mb-7 mb-2">
         <p className="font-bold text-2xl">Email</p>
         <input
           type="email"
           onChange={handleChange}
           placeholder="email"
           name="email"
-          className="w-96 h-10 bg-anastasia-5 rounded-xl border border-black shadow-inner px-2"
+          className="w-full sm:w-96 h-10 bg-anastasia-5 rounded-xl border border-black shadow-inner px-4"
         />
       </div>
-      <div className="text-center flex flex-col items-center gap-2 mb-20">
+
+      {/* Password Field */}
+      <div className="text-center flex flex-col items-center gap-2 lg:mb-7 mb-5">
         <p className="font-bold text-2xl">Password</p>
-        <div className="relative">
+        <div className="relative w-full sm:w-96">
           <input
             type={showPassword ? "text" : "password"}
             onChange={handleChange}
             placeholder="password"
             name="password"
-            className="w-96 h-10 bg-anastasia-5 rounded-xl border border-black shadow-inner px-2"
+            className="w-full h-10 bg-anastasia-5 rounded-xl border border-black shadow-inner px-4"
           />
           <button
             className="absolute top-3 right-4"
@@ -47,19 +55,31 @@ const FormLogin = () => {
           </button>
         </div>
       </div>
+
+      {/* Login Button */}
       <button
         onClick={handleLogin}
-        className={`w-96 h-10 bg-anastasia-4 rounded-xl border border-black [box-shadow:5px_5px_black]  text-anastasia-2 font-semibold text-2xl px-4 mb-10 ${
+        className={`lg:w-full md:w-96 w-56 h-10 bg-anastasia-4 rounded-xl border border-black place-items-center text-anastasia-2 font-semibold text-2xl px-4 mb-10 ${
           isButtonDisabled
             ? "opacity-75"
-            : "opacity-100 active:[box-shadow:0px_0px_black]"
+            : "active:shadow-none active:translate-x-[3px] active:translate-y-[3px] shadow-[5px_5px_0px_black]"
         }`}
         disabled={isButtonDisabled}
       >
-        LOGIN
+        {loading ? (
+          <AiOutlineLoading3Quarters className="animate-spin" />
+        ) : (
+          "LOGIN"
+        )}
       </button>
-      <p>
-        Don't have any account?{" "}
+
+      {success && (
+        <p className="text-2xl font-semibold text-black">{success}</p>
+      )}
+      {error && <p className="text-2xl font-semibold text-red-600">{error}</p>}
+      {/* Registration Link */}
+      <p className="text-center ">
+        Don't have an account?{" "}
         <Link href="/register" className="hover:underline">
           Register Here!
         </Link>
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import FormLogin from "@/components/FormLogin";
 import Image from "next/image";
-import Link from "next/link";
 
 const LoginPage = () => {
   return (
-    <div className="flex justify-center items-center gap-2 text-black bg-anastasia-1 h-screen px-[44px] pt-[20px] pb-[100px] lg:pl-[29px] lg:pr-[42px] lg:py-[20px] ">
+    <div className="flex flex-col lg:flex-row lg:justify-center lg:items-center justify-evenly items-center gap-2 text-black bg-anastasia-1 lg:h-screen min-h-screen px-[30px] pt-[20px] pb-[10px] lg:pl-[29px] lg:pr-[42px] lg:py-[20px] ">
       <div className="w-full flex flex-col justify-between items-center gap-4">
-        <Image src="/Logo.png" alt="Logo" width={400} height={400} />
-        <h1 className="text-4xl font-bold mb-4">
+        <Image
+          src="/Logo-crop.png"
+          alt="Logo"
+          width={400}
+          height={400}
+          className="w-48 h-auto sm:w-64 md:w-80 lg:w-96 xl:w-[400px] mx-auto"
+        />
+        <h1 className="lg:text-4xl text-xl text-center font-bold">
           Capturing the Spark of Joy in Every Blink!
         </h1>
-        <Link href="/">
-          <div className="rounded-2xl border-2 border-dashed border-black bg-white px-6 py-3 font-semibold uppercase text-black transition-all duration-300 hover:translate-x-[-4px] hover:translate-y-[-4px] hover:rounded-md hover:shadow-[4px_4px_0px_black] active:translate-x-[0px] active:translate-y-[0px] active:rounded-2xl active:shadow-none">
-            <h3 className="text-xl text-black">Return to Landing Page!</h3>
-          </div>
-        </Link>
       </div>
       <FormLogin />
     </div>
